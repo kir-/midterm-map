673ef3d9ff6c3ea3114930959f070cc64ee90f3b
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-07-28 01:03:52 +0000

    added google api (geolocation, maps javascript)

---
+++ public/scripts/google_map_api.js
@@ -0,0 +1,41 @@
+$(() => {
+  const loadMap = function(location, element) {
+    let newMap = new google.maps.Map(element, {
+      center: location,
+      zoom: 16
+    });
+    new google.maps.Marker({
+      position: location,
+      map: newMap,
+      title: 'Hello World!'
+    });
+  };
+
+  const getGeolocation = function(callback) {
+    $.ajax({
+      method:'POST',
+      url:'https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCS2HA8sY280xwjwAZbVRoA5hIzfDg41xM'
+    }).done((location) => {
+      callback(location.location)
+    })
+  }
+
+  $showGeolocation = $('#showGeolocation');
+  $showGeolocation.on('click', (event) => {
+    event.preventDefault();
+    getGeolocation((location) => {
+      console.log(location)
+      $(`<div>${'lat: ' + location.lat + ',' + 'lng: ' + location.lng}</div>`).appendTo('#toShowLoc')
+    })
+  })
+
+  
+
+  $showmap = $('#showmap');
+  $showmap.on('click', (even) => {
+    const mapElement = $('.map')[0];
+    const myloc = {lat: 49.2807762, lng: -123.022516000000};
+    loadMap(myloc, mapElement);
+  })
+
+})
