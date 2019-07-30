637e11b1b1e39d330d04956717b93eab954fa916
Sean-HL-Lin <hongshenglin92@gmail.com>
2019-07-30 17:30:36 +0000

    added fake data into database and deleted unused schema

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,12 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+INSERT INTO users (name, password) VALUES ('grayson', '123' );
+INSERT INTO users (name, password) VALUES ('sean', '123' );
+
+
+
+INSERT INTO groups (name) VALUES ('Best_of_The_Best');
+
+INSERT INTO members (member_id, group_id, edit) VALUES ('1', '1', true);
+INSERT INTO members (member_id, group_id, edit) VALUES ('2', '1', true);
+
+
